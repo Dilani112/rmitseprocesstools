10bcf9c2eec0ff35503fee7b1d4ace39bf5ca439
Thejana Satanarachchi <thejanasatanarachchi@outlook.com>
2017-03-28 18:31:19 +1100

    Added User Class

---
+++ AppointmentSystem.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Tue Mar 28 16:39:02 AEDT 2017
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class AppointmentSystem extends JFrame {
+    public AppointmentSystem() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
