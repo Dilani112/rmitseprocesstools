17dc73b4daa8577de4642fc03fe17cd8134b3022
Thejana Satanarachchi <thejanasatanarachchi@outlook.com>
2017-04-08 17:33:24 +1000

    Fixed project structure for projectUI

--- src/view/App.java
+++ src/view/App.java
@@ -1,9 +1,8 @@
-/*
+package view;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projectUI.sept;
 
 import java.util.Scanner;
 /**
