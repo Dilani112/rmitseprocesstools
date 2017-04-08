2968e2b1f9f13682a86364ba6ad259346684c116
Thejana Satanarachchi <thejanasatanarachchi@outlook.com>
2017-04-08 17:24:20 +1000

    fixed dir structure

---
+++ src/model/Booking.java
@@ -0,0 +1,9 @@
+package rmitseprocesstools.model;
+
+import java.util.ArrayList;
+
+public class Booking {
+	public int BookingId;
+	public ArrayList<Integer> ScheduleIds;
+	public BookingStatus Status;
+}
---
+++ src/model/User.java
@@ -0,0 +1,6 @@
+package rmitseprocesstools.model;
+
+public abstract class User {
+	public String Username;
+	public String Password;
+}
