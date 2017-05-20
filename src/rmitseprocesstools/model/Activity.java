9f58111bb4b7cf938768c6e0b5159639eff01ab9
lipyang0825 <s3613979@student.rmit.edu.au>
2017-05-20 18:19:31 +1000

    Implemented the issues on github
    able to add and edit activities, created another form for business
    registration

--- src/rmitseprocesstools/model/Activity.java
+++ src/rmitseprocesstools/model/Activity.java
@@ -1,4 +1,5 @@
 package rmitseprocesstools.model;
+import rmitseprocesstools.Utility;
 
 import java.time.LocalDateTime;
 
@@ -7,5 +8,27 @@ public class Activity {
         public int ActivityId;
         public int BusinessId;	
 	public String Name;
-	public int Duration;
+        public int Duration;
+        
+        Utility utility = new Utility();
+        
+        public boolean setActivityName(String input) {
+        if(utility.validateName(input)){
+            Name = input;
+            return true;
+        }
+        return false;
+    }
+        
+        public boolean setDuration(String input) {
+        try{
+            Duration = Integer.parseInt(input);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+        
+        
+        
 }
